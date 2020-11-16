a512b92ff50a9b8dd7c27869e66b51e34ac0101c
Whey <golova1813@yandex.ru>
2020-11-16 17:58:24 +0300

    1

--- js/main.js
+++ js/main.js
@@ -8,3 +8,18 @@
         }
     };
 }());
+
+
+
+// Burger
+(function(){
+    const burgerItem=document.querySelector('.burger');
+    const menu=document.querySelector('.header__nav');
+    const menuClose=document.querySelector('.header__nav-close');
+    burgerItem.addEventListener('click', ()=>{
+    menu.classList.add('header__nav-active');
+    });
+    menuClose.addEventListener('click', ()=>{
+        menu.classList.remove('header__nav-active');
+        });
+}());
